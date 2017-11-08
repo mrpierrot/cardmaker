7ddc43fb8c0882aad360a444523b379cd275c9ed
Pierre Chabiland <pierre@casusludi.com>
2017-11-08 15:25:20 +0100

    Parse GSheet to json format

--- src/index.js
+++ src/index.js
@@ -1,21 +1,71 @@
 "use strict"
 
 const path = require('path');
-const {readFile,parseJSON} = require('./utils');
+const {readFile,parseJSON,readSpreadsheet} = require('./utils');
+const _ = require('lodash');
 
 const CONF_FILE_NAME = 'deckcards.json';
 
 const currentPath = process.cwd();
 
+/**
+ * Read the conf file to the "dir" path
+ * @param {string} dir 
+ * @return {Promise}
+ */
 function getConf(dir) {
     const confFilePath = path.join(dir, CONF_FILE_NAME);
     return readFile(confFilePath).then(parseJSON);
 }
 
-function parseGSheet(credentials) {
+/**
+ * Format GSheet cells to a readable template format
+ * @param {array} cells 
+ * @return {object} a key/value object
+ */
+function formatCells(cells){
+    if(cells.length==0)return {}
+    const varNames = _.head(cells);
+    const varValuesByLine = _.tail(cells);
+    return varValuesByLine.map( 
+        line => _.reduce(line,(res,value,index) => {
+            res[varNames[index]] = value
+            return  res;
+        },{}) 
+    )
+}
 
+/**
+ * Format Google SpreadSheet sheets to an array of templates object dictionary 
+ * @param {array} sheets an array of Sheet Object
+ * @return {array}
+ */
+function formatGSheetToTemplateFiller(sheets){
+    return sheets.map( o => {
+        return {
+            id:_.kebabCase(o.sheet.title),
+            name:o.sheet.title,
+            data: formatCells(o.cells)
+        }
+    })
+}
+
+/**
+ * Read a GSheet from Google Drive and parse it to a list of templates dictionnary
+ * @param {string} crendentials the crendentials file path 
+ * @param {string} sheetId the Google SpreadSheet ID in Google Drive
+ */
+function readGSheet(credentials,sheetId) {
+    return readSpreadsheet(credentials,sheetId).then(formatGSheetToTemplateFiller);
 }
 
 getConf(currentPath).then(conf => {
     console.log(conf);
+
+    const {template, output, gsheet} = conf;
+
+    readGSheet(path.join(currentPath,gsheet.credentials),gsheet.sheetId).then( sheets => {
+        console.log(sheets[0]);
+    });
+
 });
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,14 @@
 "use strict"
 
 const fs = require('fs');
+const GoogleSpreadsheet = require("google-spreadsheet");
 
+/**
+ * Read a file from disk
+ * @param {string} file the file path
+ * @param {string} encoding the encoding system
+ * @return {Promise} a promise
+ */
 exports.readFile = function readFile(file,encoding='utf8') {
     return new Promise((resolve, reject) => {
         fs.access(file, fs.constants.R_OK, (err) => {
@@ -19,6 +26,11 @@ exports.readFile = function readFile(file,encoding='utf8') {
     });
 }
 
+/**
+ * Parse a string to JSON
+ * @param {string} data the json string
+ * @return {Promise} a promise
+ */
 exports.parseJSON = function parseJSON(data){
     return new Promise((resolve, reject) => {
         try {
@@ -29,4 +41,49 @@ exports.parseJSON = function parseJSON(data){
         }
 
     });
+}
+
+/**
+ * Read a GSheet worksheet cells and return this to json format
+ * @param {Sheet} sheet a sheet object
+ * @return {Promise} a promise
+ */
+function readWorkSheetCells(sheet) {
+    return new Promise((resolve, reject) =>
+        sheet.getCells(function (err, row_data) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            var res = [];
+            row_data.forEach(function (e) {
+                if (!(e.row - 1 in res)) res[e.row - 1] = [];
+                res[e.row - 1][e.col - 1] = e.value;
+            });
+            for (var i = 0; i < res.length; i++) {
+                if (res[i] == null) res[i] = [];
+            }
+            resolve({ sheet: sheet, cells: res });
+        })
+    );
+}
+
+/**
+ * Read a online GSheet and parse this content to json
+ * @param {string} crendentials the crendentials file path 
+ * @param {string} sheetId the Google SpreadSheet ID in Google Drive
+ * @returns {Promise} a promise
+ */
+exports.readSpreadsheet = function readSpreadsheet(credendials, sheetId) {
+    return new Promise((resolve, reject) => {
+        var sheet = new GoogleSpreadsheet(sheetId);
+        sheet.useServiceAccountAuth(credendials, function (err) {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            sheet.getInfo((err, { worksheets }) => resolve(Promise.all(worksheets.map((worksheet, index) => readWorkSheetCells(worksheet)))));
+        });
+    })
 }
