547cbc3b45bcf50a1eee5f0c7f14364b3d2aa6d1
Pierre Chabiland <pierre@casusludi.com>
2017-11-10 21:48:51 +0100

    writing test in progress

---
+++ test/utils.spec.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const GoogleSpreadsheet = require("google-spreadsheet");
+
+const utils = require('../lib/utils');
+
+const PUBLIC_SHEET_ID = '1QJm95kTdpR9XT6fC7sirsPRVFjOOri74-jH3mSd1gf8';
+
+describe('utils', function () {
+    describe('#readFile()', function () {
+        it('read a file with content "mox"', function (done) {
+            utils.readFile(__dirname + '/assets/file.txt')
+                .then(data => {
+                    assert.equal('mox', data);
+                    done();
+                });
+        });
+    });
+    describe('#parseJSON()', function () {
+        it('parse a JSON string', function (done) {
+            utils.parseJSON('{"foo":1}')
+                .then(data => {
+                    assert.equal(1, data.foo);
+                    done();
+                });
+        });
+    });
+    describe('#getSpreadSheetAccess()', function () {
+        const sheet = new GoogleSpreadsheet(PUBLIC_SHEET_ID);
+        it('get public access', function (done) {
+            utils.getSpreadSheetAccess(sheet)
+                .then(data => {
+                    assert.ok(true);
+                    done();
+                });
+        });
+    });
+});
